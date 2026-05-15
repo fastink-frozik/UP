96a1b54e6e4d52b5cafeb34886f7f47cfd3b86df
fastink-frozik <fastink777@gmail.com>
2026-05-15 14:24:28 +0500

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('magaz.db')
-cursor = conn.cursor()
-try:
-    cursor.execute("""SELECT * FROM user """)
-    print(cursor.fetchall())
-except:
-    print('mau')
